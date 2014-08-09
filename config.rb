eac7d8bfdcb8aee97d3c23b96232efdd9e2fd9e6
KITE <ixkaito@gmail.com>
2014-08-09 18:24:28 +0900

    Improve the config.rb file

--- config.rb
+++ config.rb
@@ -1,10 +1,12 @@
+# run `$ compass compile -e production --force` to force compile for production
+
 http_path = "/"
 preferred_syntax = :scss
 css_dir = "assets/css"
 sass_dir = "assets/_sass"
 images_dir = "assets/images"
 javascripts_dir = "assets/js"
-output_style = :compressed # :expanded, :nested, :compact or :compressed
 # relative_assets = true
-# line_comments = false
 # sass_options = { :debug_info => true }
+output_style = ( environment == :production ) ? :compressed : :expanded # :expanded, :nested, :compact or :compressed
+line_comments = ( environment == :production ) ? false : true
