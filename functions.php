d668511f566bf934f7c85162e01f964a67f87540
KITE <ixkaito@gmail.com>
2014-08-15 03:21:34 +0900

    Change ":", "endif" to "{", "}"

--- functions.php
+++ functions.php
@@ -6,8 +6,9 @@
 */
 function get_head_title() {
 	$title = '';
-	if ( is_singular() )
+	if ( is_singular() ) {
 		$title .= wp_title( '|', false, 'right' );
+	}
 	$title .= get_bloginfo( 'name' );
 	return $title;
 }
@@ -22,13 +23,14 @@ function head_title() {
 ================================================================================
 */
 function get_meta_description() {
-	if ( is_singular() && have_posts() ):
-		while(have_posts()): the_post();
+	if ( is_singular() && have_posts() ) {
+		while( have_posts() ) {
+			the_post();
 			$description = mb_substr( get_the_excerpt(), 0, 100 );
-		endwhile;
-	else:
+		}
+	} else {
 		$description = get_bloginfo( 'description' );
-	endif;
+	}
 	return $description;
 }
 
@@ -43,41 +45,42 @@ function meta_description() {
 */
 function get_og( $arg ) {
 
-	if ( $arg === 'type' ):
+	if ( $arg === 'type' ) {
 		$og = is_front_page() ? 'website' : 'article';
 
-	elseif ( $arg === 'url' ):
+	} elseif ( $arg === 'url' ) {
 		$og = is_404() ? home_url() : set_url_scheme( 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 
-	elseif ( $arg === 'title' ):
-		if ( is_singular() && have_posts() ):
-			while( have_posts() ): the_post();
+	} elseif ( $arg === 'title' ) {
+		if ( is_singular() && have_posts() ) {
+			while( have_posts() ) {
+				the_post();
 				$title = the_title( '', '', false );
-			endwhile;
-		else:
+			}
+		} else {
 			$title = get_bloginfo( 'name' );
-		endif;
+		}
 		$og = $title;
 
-	elseif ( $arg === 'description' ):
+	} elseif ( $arg === 'description' ) {
 		$og = get_meta_description();
 
-	elseif ( $arg === 'image' ):
+	} elseif ( $arg === 'image' ) {
 		global $post;
 		$post_content   = ( ! is_archive() && ! is_front_page() && ! is_home() ) ? $post->post_content : null;
 		$search_pattern = '/<img.*?src=(["\'])(.+?)\1.*?>/i';
-		if ( has_post_thumbnail() && ! is_archive() && ! is_front_page() && ! is_home() ):
+		if ( has_post_thumbnail() && ! is_archive() && ! is_front_page() && ! is_home() ) {
 			$image_id   = get_post_thumbnail_id();
 			$image      = wp_get_attachment_image_src( $image_id, 'full' );
 			$og = $image[0];
-		elseif ( preg_match( $search_pattern, $post_content, $imgurl ) && ! is_archive() && ! is_front_page() && ! is_home() ):
+		} elseif ( preg_match( $search_pattern, $post_content, $imgurl ) && ! is_archive() && ! is_front_page() && ! is_home() ) {
 			$og = $imgurl[2];
-		else:
+		} else {
 			$og = get_template_directory_uri() . '/assets/images/og-default.png';
-		endif;
-	else:
+		}
+	} else {
 		$og = '';
-	endif;
+	}
 
 	return $og;
 
--- header.php
+++ header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>		<html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 7]>			<html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
-<!--[if IE 8]>			<html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
-<!--[if gt IE 8]><!-->	<html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
+<!--[if lt IE 7]>       <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 7]>          <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>><![endif]-->
+<!--[if IE 8]>          <html class="no-js lt-ie9" <?php language_attributes(); ?>><![endif]-->
+<!--[if gt IE 8]><!-->  <html class="no-js" <?php language_attributes(); ?>><!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta http-equiv="Content-Style-Type" content="text/css" />
