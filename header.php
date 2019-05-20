7e1d7f2a4d98f8241785ce430f13835c2d312acc
Kite <ixkaito@gmail.com>
2019-05-21 00:09:36 +0900

    Refactoring

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <?php wp_head(); ?>
