08fe87a8170e521baf74f85bbd5b1264dfcd8dcc
Kite <ixkaito@gmail.com>
2020-07-21 10:52:33 +0900

    Use js instead of tsx

---
+++ src/components/header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+
+import '../css/main.css'
+
+const Header = ({ siteTitle }) => (
+  <header>
+    <h1 className="text-white">
+      <Link to="/">
+        <img src="" alt=""/>
+        {siteTitle}
+      </Link>
+    </h1>
+    <nav>
+      <ul>
+        <li><a href="#getting-started">Getting Started</a></li>
+        <li><a href="#configurations">Configurations</a></li>
+      </ul>
+    </nav>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,9 +11,13 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+    <figure
+      css={{
+        background: '#eee',
+      }}
+    >
       <Image />
-    </div>
+    </figure>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
