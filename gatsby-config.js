0337d7bb6ec7d3747c60fb0066a9f9bf307dca48
Kite <ixkaito@gmail.com>
2020-07-24 14:12:28 +0900

    Chagne the theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,8 +21,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0080ff`,
+        theme_color: `#0080ff`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
