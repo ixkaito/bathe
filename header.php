a3cfff3ba117ef2562c608e1ffcc4f80e21e9b9e
Kite <ixkaito@gmail.com>
2023-06-20 14:05:08 +0900

    Add some initial styles

--- header.php
+++ header.php
@@ -7,15 +7,16 @@
 <?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class( 'antialiased flex flex-col min-h-screen' ); ?>>
 <?php wp_body_open(); ?>
 
 <header class="header">
 	<h1>
-		<a class="text-blue-500 hover:underline" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<a class="font-bold text-blue-500 hover:underline" href="<?php echo esc_url( home_url( '/' ) ); ?>">
 			<?php bloginfo( 'name' ); ?>
 		</a>
 	</h1>
 </header>
 
-<main id="primary" class="site-main" role="main">
+<div class="lg:flex grow">
+	<main id="primary" class="grow p-8" role="main">
--- template-parts/loop.php
+++ template-parts/loop.php
@@ -5,7 +5,7 @@
 		<?php while ( have_posts() ) : the_post(); ?>
 
 			<li>
-				<h1>
+				<h1 class="text-2xl font-bold hover:underline">
 					<a href="<?php the_permalink(); ?>">
 						<?php the_title(); ?>
 					</a>
