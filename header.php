7a704d1eb7251043dd88e5bf666a89e4205e633a
Kite <ixkaito@gmail.com>
2023-06-20 14:40:17 +0900

    Remove X-UA-Compatible

--- header.php
+++ header.php
@@ -2,7 +2,6 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <?php wp_head(); ?>
 </head>
