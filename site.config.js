edeadf8a6bf5031dd7afa70286ab2714993568ea
osgsm <50955473+oshimax@users.noreply.github.com>
2023-07-03 10:37:46 +0900

    Add "Tailwind CSS" to descriptions

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ export const config = {
   pathPrefix: '/bathe',
   siteMetadata: {
     title: `Bathe`,
-    description: `The simplest WordPress starter theme including full setup for Sass, PostCSS, Autoprefixer, stylelint, Vite, ESLint, imagemin, Browsersync, etc.`,
+    description: `The simplest WordPress starter theme including full setup for Tailwind CSS, Sass, PostCSS, Autoprefixer, stylelint, Vite, ESLint, imagemin, Browsersync, etc.`,
     url: `https://ixkaito.github.io/bathe`,
     author: `@ixkaito`,
   },
